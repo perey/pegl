6b97b7f7665b6600ad9311a47aaee53d843abe32
Tim Pederick <pederick@gmail.com>
2014-03-16 00:37:50 +0800

    Fixed some errors in using or referring to release_thread().

--- pegl/display.py
+++ pegl/display.py
@@ -94,7 +94,7 @@ class Display:
         from all other threads in which this display has been used.
 
         '''
-        release_thread()
+        self.release_thread()
         self.terminate()
 
     def __eq__(self, other):
@@ -225,10 +225,11 @@ class Display:
     def terminate(self):
         '''Invalidate all resources associated with this display.
 
-        The display handle itself remains valid, and so its release()
-        method can still be called in any threads that have used it. The
-        display can even be reinitialized (by calling initialize()),
-        though the terminated resources will not be made valid again.
+        The display handle itself remains valid, and so its
+        release_thread() method can still be called in any threads that
+        have used it. The display can even be reinitialized (by calling
+        initialize()), though the terminated resources will not be made
+        valid again.
 
         It is not generally necessary to call this function directly, as
         it is called by the display's destructor method. The only
