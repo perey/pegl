4977866dbf5c99ace46d8d1ba03448a81dae8eef
Tim Pederick <pederick@gmail.com>
2014-03-22 00:30:58 +0800

    Updated setup script.

--- setup.py
+++ setup.py
@@ -1,8 +1,49 @@
+# Setup file for Pegl.
+#
+# Copyright © 2012-14 Tim Pederick.
+# 
+# Some parts based on the PyPA sample project:
+#   https://github.com/pypa/sampleproject/
+# Copyright © 2013-14 The Python Packaging Authority.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Standard library imports.
+import os.path
+import re
 from distutils.core import setup
+
+version_re = re.compile('''^\s*__version__ = ['"]([^'"]+)['"]''')
+
+def get_version(package, filename='__init__.py'):
+    '''Find the __version__ attribute of the specified package.'''
+    with open(os.path.join(package, filename)) as f:
+        for line in f:
+            match = version_re.search(line)
+            if match:
+                return match.group(1)
+    raise RuntimeError('no version information found')
 
 setup(
     name='Pegl',
-    version='0.1a4_1.4',
+    version=get_version('pegl'),
     author='Tim Pederick',
     author_email='pederick@gmail.com',
     packages=['pegl', 'pegl.attribs', 'pegl.ext'],
