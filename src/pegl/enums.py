b2e1cee3514c1bde5d7b7963096a79c80fadb919
Tim Pederick <pederick@gmail.com>
2020-09-27 22:49:40 +0800

    Refitted enums with hex repr

--- src/pegl/enums.py
+++ src/pegl/enums.py
@@ -18,15 +18,65 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Pegl. If not, see <http://www.gnu.org/licenses/>.
+#
+# The function hex_repr is based on the "aenum" package:
+#     Copyright (c) 2015, 2016, 2017, 2018 Ethan Furman.
+#     All rights reserved.
+#
+#     Redistribution and use in source and binary forms, with or without
+#     modification, are permitted provided that the following conditions
+#     are met:
+#
+#         Redistributions of source code must retain the above
+#         copyright notice, this list of conditions and the
+#         following disclaimer.
+#
+#         Redistributions in binary form must reproduce the above
+#         copyright notice, this list of conditions and the following
+#         disclaimer in the documentation and/or other materials
+#         provided with the distribution.
+#
+#         Neither the name Ethan Furman nor the names of any
+#         contributors may be used to endorse or promote products
+#         derived from this software without specific prior written
+#         permission.
+#
+#     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#     "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#     LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#     FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#     COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#     INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#     BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#     CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#     LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#     ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#     POSSIBILITY OF SUCH DAMAGE.
 
 # Standard library imports.
-from aenum import IntEnum, IntFlag, extend_enum
+from aenum import IntEnum, IntFlag, extend_enum, _decompose
+# I sure hope importing _decompose doesn't come back to bite me...
 
 # Local imports.
 from . import egl
 
 __all__ = ['ConfigAttrib', 'ConfigCaveat', 'NativeEngine', 'ReadOrDraw',
            'SurfaceAttrib', 'SurfaceTypeFlag', 'TransparentType']
+
+# Use hexadecimal in the repr to make it easier to compare to the EGL headers.
+def hex_repr(self):
+    cls = self.__class__
+    if self._name_ is not None:
+        return '<{}.{}: {:#06x}>'.format(cls.__name__, self._name_,
+                                         self._value_)
+    members, uncovered = _decompose(cls, self._value_)
+    return '<{}.{}: {:#06x}>'.format(cls.__name__,
+                                     '|'.join([str(m._name_ or m._value_)
+                                               for m in members]),
+                                     self._value_)
+IntEnum.__repr__ = hex_repr
+IntFlag.__repr__ = hex_repr
 
 # EGL 1.0 enumerations.
 class ConfigAttrib(IntEnum):
